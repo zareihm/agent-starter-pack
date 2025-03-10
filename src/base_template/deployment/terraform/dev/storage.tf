60d17faf7ec9eccbba6212ae6e5a87cc73cc615c
eliasecchig <115624100+eliasecchig@users.noreply.github.com>
2025-03-10 20:05:08 +0100

    fix: terraform dev storage (#20)

--- src/base_template/deployment/terraform/dev/storage.tf
+++ src/base_template/deployment/terraform/dev/storage.tf
@@ -22,18 +22,6 @@ resource "google_storage_bucket" "logs_data_bucket" {
     prevent_destroy = false
     ignore_changes  = all
   }
-
-  # Use this block to create the bucket only if it doesn't exist
-  count      = length(data.google_storage_bucket.existing_bucket) > 0 ? 0 : 1
-  depends_on = [resource.google_project_service.services]
-}
-
-data "google_storage_bucket" "existing_bucket" {
-  name    = "${var.dev_project_id}-logs-data"
-  project = var.dev_project_id
-
-  # Capture the error if the bucket doesn't exist
-  count      = can(data.google_storage_bucket.existing_bucket[0]) ? 1 : 0
   depends_on = [resource.google_project_service.services]
 }
 
